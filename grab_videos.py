339fe3b55529fbc154a7196240189106f37d55c4
misraturp <misra@assemblyai.com>
2024-07-31 14:39:52 +0200

    theme customization and changin the layout

--- grab_videos.py
+++ grab_videos.py
@@ -1,32 +1,42 @@
 import requests
 import json
 
+
 def extract_video_data(video_json):
     title = video_json["snippet"]["title"]
     link = f'https://www.youtube.com/watch?v={video_json["id"]["videoId"]}'
     description = video_json["snippet"]["description"]
     thumbnail = video_json["snippet"]["thumbnails"]["medium"]["url"]
     channelName = video_json["snippet"]["channelTitle"]
-    channelLink = f'https://www.youtube.com/channel/{video_json["snippet"]["channelId"]}'
+    channelLink = (
+        f'https://www.youtube.com/channel/{video_json["snippet"]["channelId"]}'
+    )
 
-    video = {"video_selected":"False" ,"video_thumbnail":thumbnail, "video_title":title, "video_desc":description,
-              "channel_name":channelName, "video_link":link, "channel_link":channelLink}
-    
+    video = {
+        "video_selected": "False",
+        "video_thumbnail": thumbnail,
+        "video_title": title,
+        "channel_name": channelName,
+        "video_desc": description,
+        "video_link": link,
+        "channel_link": channelLink,
+    }
+
     return video
 
+
 def search_yt(api_key, search_phrase):
-        
-    search_url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&q={search_phrase}&key={api_key}'
+    search_url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q={search_phrase}&key={api_key}"
     json_video_info = requests.get(search_url)
 
     video_data = json.loads(json_video_info.text)
-    raw_search_results = video_data.get('items')
-        
+    raw_search_results = video_data.get("items")
+
     refined_results = refine_results(raw_search_results)
     return refined_results
 
-def refine_results(raw_results):
 
+def refine_results(raw_results):
     all_videos = []
 
     for res in raw_results:
--- strip_audio.py
+++ strip_audio.py
@@ -1,15 +1,17 @@
 import yt_dlp
 
+
 def download_save_audio(link):
-  
-  print("Downloading audio")
-  
-  filename = link[-11:]+'.mp3'
+    print("Downloading audio")
 
-  with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': filename}) as video:
-    info_dict = video.extract_info(link, download = True)
-    video_title = info_dict['title']
-    print(video_title)
-    video.download(link)
+    filename = link[-11:] + ".mp3"
 
-    return filename
+    with yt_dlp.YoutubeDL(
+        {"extract_audio": True, "format": "bestaudio", "outtmpl": filename}
+    ) as video:
+        info_dict = video.extract_info(link, download=True)
+        video_title = info_dict["title"]
+        print(video_title)
+        video.download(link)
+
+        return filename
